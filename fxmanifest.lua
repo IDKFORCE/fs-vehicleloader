7951645d1cd67ec2444874e5c1561670f805e63c
GhostFace Scripts <71199348+IDKFORCE@users.noreply.github.com>
2024-11-09 01:31:48 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,14 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author '⸸♱♥IDK_FORCE♥♱⸸#7611'
-description 'fs-vehicleloader'
+author 'GhostFaceScripts'
+description 'GhostFace VehicleLoader'
+version '1.0.0'
 
 shared_script 'config.lua'
 client_script 'client.lua'
 server_script 'server.lua'
 
 fivem_checker 'yes'
+
+lua54 'yes'
